63bd6c3f2ecef94f1ba491562ddad972038a5dd6
Robson Mendonça <rob.ams@gmail.com>
2020-05-25 19:27:44 -0300

    Remover dados sensiveis!

--- src/models/Cliente.js
+++ src/models/Cliente.js
@@ -4,10 +4,9 @@ const Cliente = mongoose.model('cliente', {
   nome: {type: String, required: true},
   sobrenome: {type: String},
   cpf: {type: String, required: true, unique: true},
-  senha: {type: String, required: true},
   login: {type: String, required: true, unique: true},
   createat: {type: Date, default:new Date().getDate() },
   updatetat: {type: Date, required: true, default:new Date().getDate() }
 });
 
-module.exports = Cliente;
+module.exports = Cliente;tatu
