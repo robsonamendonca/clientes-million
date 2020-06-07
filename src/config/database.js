4d2172c90fdd0d95117368e13522742a18623505
Robson Mendonça <rob.ams@gmail.com>
2020-06-07 17:37:31 -0300

    Ajuste dados sensivel

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/clienteMillion';
+const uri = 'mongodb://localhost:27017/clienteMillion';
 
 mongoose.connect(uri,  { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.Promise = global.Promise;
